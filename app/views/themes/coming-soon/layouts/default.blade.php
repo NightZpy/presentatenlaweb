78415c933dd2e13c259ea41f6425f9c55e544cba
Lenyn Alcantara (NightZpy) <nightzpy@gmail.com>
2014-07-28 16:54:07 -0430

    Cambiados los textos, re-ordenada la pagina de home

--- app/views/themes/coming-soon/layouts/default.blade.php
+++ app/views/themes/coming-soon/layouts/default.blade.php
@@ -75,7 +75,7 @@
   <div id="pattern"><div id="gradient">
 
 	<div id="customization">	
-	  <h2>Select a color:</h2>
+	  <h2>Selecciona un color:</h2>
 	    <div class="colors">
 		  <a href="/#" class="green">#339966</a>
 		  <a href="/#" class="blue">#417E89</a>
@@ -83,21 +83,21 @@
 		  <a href="/#" class="brown">#78715F</a>
 		  <a href="/#" class="black">#555555</a>
 		</div>	  
-	  <h2>Or pick an image:</h2>
+	  <!-- <h2>Or pick an image:</h2>
 		<img src="{{asset('themes/coming-soon/img/background_thumb.jpg')}}" alt="" class="bg_image" />
 	  <hr />	  
-	  <h2>Set the content:</h2>
+	  <h2>Qué quieres ver?</h2>
 		<form>
 		  <ul>
-			<li><input type="checkbox" name="text" /> Text</li>
-			<li><input type="checkbox" name="countdown" /> Countdown</li>		
-			<li><input type="checkbox" name="progress" checked="checked" /> Progress</li>
-			<li><input type="checkbox" name="description" /> Description</li>
+			<li><input type="checkbox" name="text" /> Texto</li>
+			<li><input type="checkbox" name="countdown" /> Contador</li>		
+			<li><input type="checkbox" name="progress" checked="checked" /> Progreso</li>
+			<li><input type="checkbox" name="description" /> Descripción</li>
 			<li><input type="checkbox" name="twitter" /> Twitter</li>
-			<li><input type="checkbox" name="subscribe" /> Subscription</li>
-			<li><input type="checkbox" name="more_info" /> More info</li>
+			<li><input type="checkbox" name="subscribe" /> Suscribete</li>
+			<li><input type="checkbox" name="more_info" /> Más información</li>
 		  </ul>
-		</form>	  
+		</form>	   -->
 	</div>
 
 
@@ -121,7 +121,7 @@
 		  
 		  <!-- Custom text -->
 		  <section class="text">
-			<p>coming soon</p>
+			<p>Tu presencia es importante en Internet.</p>
 		  </section>
 		  <!-- Custom text end -->
 	  
@@ -135,9 +135,9 @@
 				$("#countdown").countdown({until: launchTime, format: "odHMS"});
 			});
 		  </script>
-		  <section class="countdown">
+		  <!-- <section class="countdown">
 			<div id="countdown"></div>
-		  </section>
+		  </section> -->
 		  <!-- Countdown end -->
 		  
 		  
@@ -149,7 +149,7 @@
 		  </script>
 		  <section class="progress">
 			<div id="progressbar"></div>
-			<h2>85%</h2> <!-- Set your progress, must be percentage -->
+			<h2>65%</h2> <!-- Set your progress, must be percentage -->
 		  </section>
 		  <!-- Progress end -->		  
 		  
@@ -158,9 +158,9 @@
 		  <section class="description">
 			<div class="description_top"></div>
 			<div class="description_content">
-			  <p>Having a coming soon site is great for new projects.</p>
-			  <p>You can tell visitors something about your new project and build a <a href="/#">community</a> even before you finish the website.</p>
-			  <p>Get new subscribers and let them know when your project is ready for launch.</p>
+			  <p>Pronto estaremos ofreciendo un excelente servicio.</p>
+			  <p>Tenemos proyectos SaaS en mente y en desarrollo para su empresa, a la medida y personalizados.</p>
+			  <!-- <p>Get new subscribers and let them know when your project is ready for launch.</p> -->
 			</div>
 			<div class="description_bottom"></div>
 		  </section>		  
@@ -180,9 +180,9 @@
 				});
 			});
 		  </script>
-		  <section class="twitter">
+		  <!-- <section class="twitter">
 			<div id="tweet"></div>
-		  </section>
+		  </section> -->
 		  <!-- Twitter widget end -->
 		  
 		  
@@ -198,17 +198,17 @@
 						data: {email: $("input[name=email]").val()},
 						success: function(data) {
 							if (data == "successful") {
-								$(".subscribe p").html("Thanks for your interest! We will let you know.").slideDown();
+								$(".subscribe p").html("Gracias por suscribirte, pronto te estaremos dando a conocer nuestros servicios-").slideDown();
 							} else if (data == "already_subscribed") {
-								$(".subscribe p").html("This email is already subscribed.").slideDown();
+								$(".subscribe p").html("Este email ya está registrado.").slideDown();
 							} else if (data == "invalid_email") {
-								$(".subscribe p").html("This email is invalid.").slideDown();
+								$(".subscribe p").html("Este email es inválido.").slideDown();
 							} else {
-								$(".subscribe p").html("Something went wrong. Please try again.").slideDown();	
+								$(".subscribe p").html("Algo salió mal, por favor intenta de nuevo.").slideDown();	
 							}
 						},
 						error: function () {
-							$(".subscribe p").html("Subscription is not available.").slideDown();
+							$(".subscribe p").html("La suscripción no está disponible.").slideDown();
 						}
 					});
 				});
@@ -217,8 +217,8 @@
 		  <section class="subscribe">
 			<p></p>
 			<form method="post">
-			  <input name="email" placeholder="Your e-mail address...">
-			  <input type="submit" class="submit" value="Notify me">
+			  <input name="email" placeholder="Tu e-mail...">
+			  <input type="submit" class="submit" value="Mantenerme informado.">
 			</form>
 		  </section>
 		  <!-- Subscription form end -->
@@ -239,114 +239,18 @@
 				});
 			});
 		  </script>
-		  <section class="more_info">
-			<a href="/#" id="show_more_info" class="button">More information</a>
-		  </section>
+		  <!-- <section class="more_info">
+			<a href="/#" id="show_more_info" class="button">Más información</a>
+		  </section> -->
 		  <!-- More info, part 1 end -->
 		  
 		  
 	  </div>
 	  
 	</div>
-	<!-- Main page end -->
-	
-	<!-- More info, part 2 -->
-	<div id="more_info_page"><div class="dark_background"></div>
-		
-	  <a href="/#" id="show_main" class="button">Back</a><div class="clearfix"></div>
-	
-	  <!-- About -->
-	  <section class="about">
-	  
-		<h2>About</h2>
-		
-		<!-- First paragraph -->		
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam porta cursus nunc, sit amet gravida orci fermentum id.</p>
-		<p>Donec eu dolor arcu. Duis venenatis imperdiet justo vitae dignissim. <a href="/#">Lorem ipsum</a> dolor sit amet, consectetur adipiscing elit.</p>
-		<!-- First paragraph end -->
-	  
-	    <!-- Second paragraph -->
-		<h3>Ut elementum turpis</h3>
-		<p>Curabitur adipiscing nisl urna, <a href="/#">vulputate</a> convallis quam. Integer vulputate mi quis risus feugiat eu condimentum tortor sodales.</p>
-		<p>In ut libero ac turpis fringilla venenatis eu id nulla. In eget felis ligula.</p>
-		<!-- Second paragraph end -->
-	  
-	    <!-- Third paragraph -->
-		<h3>Nam porttitor pharetra</h3>
-		<p>Nam porttitor pharetra risus, ac commodo mi malesuada pharetra. Integer convallis vehicula urna, vel luctus lacus interdum at.</p>
-		<!-- Third paragraph end -->
-	  	
-		<!-- Social icons -->
-		<h3>Social</h3>
-		<a href="index.html"><img src="{{asset('themes/coming-soon/img/facebook.png')}}" alt="Facebook" class="social" /></a>
-		<a href="index.html"><img src="{{asset('themes/coming-soon/img/twitter.png')}}" alt="Twitter" class="social" /></a>
-		<a href="index.html"><img src="{{asset('themes/coming-soon/img/linkedin.png')}}" alt="LinkedIn" class="social" /></a>
-		<a href="index.html"><img src="{{asset('themes/coming-soon/img/flickr.png')}}" alt="Flickr" class="social" /></a>
-		<a href="index.html"><img src="{{asset('themes/coming-soon/img/youtube.png')}}" alt="YouTube" class="social" /></a>
-		<!-- Social icons end -->
-		
-	  </section>
-	  <!-- About end -->
-	  
-	
-	  <!-- Contact -->
-	  <section class="contact">
-	  
-		<h2>Contact</h2>
-		
-		<!-- First paragraph -->	
-		<p>In ut libero ac turpis fringilla venenatis eu id nulla. In eget felis ligula.</p>	
-		<!-- First paragraph end -->
-
-		
-		<!-- First person contact card -->
-		<p class="person vcard">
-		  <img src="{{asset('themes/coming-soon/img/avatar.png')}}" alt="John Doe" class="photo" />
-		  <span class="fn n">John Doe <em>project manager</em></span>
-		  <span class="email">manager@coming.soon</span>
-		  <span class="tel">+421 000 111 222</span>
-		  <span class="clearfix"></span>
-		</p>
-		<!-- First person contact card end -->
-	  
-	  
-	    <!-- Second person contact card -->
-		<p class="person vcard">
-		  <img src="{{asset('themes/coming-soon/img/avatar.png')}}" alt="John Smith" class="photo" />
-		  <span class="fn n">John Smith <em>designer</em></span>
-		  <span class="email">designer@coming.soon</span>
-		  <span class="tel">+421 111 222 333</span>
-		  <span class="clearfix"></span>
-		</p>
-		<!-- Second person contact end -->
-	  
-		<div class="clearfix"></div>
-	  
-		<!-- Address -->
-		<script>
-		  $(function() {
-			  marker = "Bratislava Castle, Bratislava"; // Set the address for marker
-			  $("#map").attr("href", "http://maps.google.com/maps?q=" + escape(marker));
-			  $("#map img").attr("src", "http://maps.google.com/maps/api/staticmap?markers=" + escape(marker) + "&size=360x190&sensor=false");
-		  });
-		</script>
-		<h3>Address</h3>
-		<figure class="address vcard">
-		  <a href="index.html" id="map"><img src="{{asset('themes/coming-soon/img/map_placeholder.jpg')}}" alt="Map" /></a>
-		  <figcaption>
-		    <span class="fn">Coming Soon Inc.</span>,
-			<span class="adr">30 Dummy Street, Town, Country</span>
-		  </figcaption>
-		</figure>
-		<!-- Address end -->
-		
-	  </section>
-	  <!-- Contact end-->
-		  
-	</div>
-	<!-- More info, part 2 end -->
-	
-  </div></div>
+	<!-- Main page end -->	
+  </div>
+  </div>
 
 <script type="text/javascript">
   var _gaq = _gaq || [];
