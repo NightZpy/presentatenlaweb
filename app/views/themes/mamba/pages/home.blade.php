892f397679f400db3df56a020048fdd18cb0cb5e
ajpadilla <ajpadilla88@gmail.com>
2014-08-04 23:44:53 -0430

    Menu principal de la pagina

---
+++ app/views/themes/mamba/pages/home.blade.php
@@ -0,0 +1,25 @@
+@extends('themes.mamba.layouts.default')
+
+@section('menu_area')
+<div class="menu-area">
+	<div id="dl-menu" class="dl-menuwrapper">
+		<button class="dl-trigger">Open Menu</button>
+			<ul class="dl-menu">
+				<li>
+					<a href="#intro">Home</a>
+				</li>
+				<li><a href="#about">About</a></li>
+				<li><a href="#services">Services</a></li>
+				<li><a href="#works">Works</a></li>
+				<li><a href="#contact">Contact</a></li>
+				<li>
+					<a href="#">Sub Menu</a>
+					<ul class="dl-submenu">
+						<li><a href="#">Sub menu</a></li>
+						<li><a href="#">Sub menu</a></li>
+					</ul>
+				</li>
+			</ul>
+	</div><!-- /dl-menuwrapper -->
+</div>	
+@stop
