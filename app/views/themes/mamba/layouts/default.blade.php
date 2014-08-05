237a56f5bff368bdf42c029a85d61d7d0512d2c1
ajpadilla <ajpadilla88@gmail.com>
2014-08-04 23:55:55 -0430

    Agregando elementos del template mamba

--- app/views/themes/mamba/layouts/default.blade.php
+++ app/views/themes/mamba/layouts/default.blade.php
@@ -1,154 +1,22 @@
 <!doctype html>
 <html class="no-js">
-<head>
-  <meta charset="utf-8">
-  
-  <!-- Page title -->
-  <title>Presentate en la web, muy pronto</title>
+	<head>
+  		<meta charset="utf-8">
+ 		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    	<!-- css -->
+    	<link href="{{asset('themes/mamba/css/bootstrap.min.css)}}" rel="stylesheet" media="screen">
+    	<link href="{{asset('themes/mamba/css/style.css)}}" rel="stylesheet" media="screen">
+		<link href="{{asset('themes/mamba/color/default.css)}}" rel="stylesheet" media="screen">
+	 	<!-- js -->	
+		<script src="{{asset('themes/mamba/js/modernizr.custom.js)}}"></script>a
+    	<script src="{{asset('themes/mamba/js/jquery.js)}}"></script>
+   	 	<script src="{{asset('themes/mamba/js/bootstrap.min.js)}}"></script>
+		<script src="{{asset('themes/mamba/js/jquery.smooth-scroll.min.js)}}"></script>
+		<script src="{{asset('themes/mamba/js/jquery.dlmenu.js)}}"></script>
+		<script src="{{asset('themes/mamba/js/wow.min.js)}}"></script>
+		<script src="{{asset('themes/mamba/js/custom.js)}}"></script> 
+ 	</head>
 
-  <link rel="shortcut icon" href="{{asset('themes/coming-soon/favicon.png')}}">
-  
-  <!-- Stylesheet -->
-  <link rel="stylesheet" href="{{asset('themes/coming-soon/css/style.css')}}"> <!-- Regular stylesheet -->
-  <link rel="stylesheet" href="{{asset('themes/coming-soon/css/green.css')}}"> <!-- Pick color scheme (black.css, blue.css, dark_green.css, green.css, white.css)-->
-  
-  <!-- Fonts -->
-  <link href="http://fonts.googleapis.com/css?family=Quattrocento" rel="stylesheet" type="text/css">
-  <link href="http://fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css">
-  <link href="http://fonts.googleapis.com/css?family=Cantarell:regular,italic,bold" rel="stylesheet" type="text/css">
-  
-  <!-- JavaScript -->
-  <script src="{{asset('themes/coming-soon/js/jquery-1.5.1.min.js')}}"></script> <!-- Load jQuery library -->
-  <script src="{{asset('themes/coming-soon/js/modernizr-1.7.min.js')}}"></script> <!-- Load Modernizr -->
-  <script>
-	$(function () {
-		
-		/* Customization */
-		$("#customization input").change(function () {
-			if ( $(this).is(":checked") ) {
-				$("." + $(this).attr("name")).slideDown();
-			} else {
-				$("." + $(this).attr("name")).slideUp();
-			}					 
-		});
-		$("#bg_images img").click(function () {
-			src = $(this).attr("alt");
-			$("body").css("background", "#000000");
-			$("#background_image").fadeOut(function () {
-				$(this).attr("src", src).fadeIn();
-			});
-		});
-		$(".colors a").each(function () {
-			$(this).css("background-color", $(this).text());
-			$(this).text("");
-		});
-		$("#customization input").each(function () {
-			if ( !$(this).is(":checked") ) {
-				$("." + $(this).attr("name")).addClass("hidden");
-			}
-		});
-		$(".colors a").click(function () {
-			color = $(this).attr("class");
-			$("#background_image").addClass("hidden");
-			$("body").removeClass("active_bg_image");
-			$("header").css("background-image", "url(themes/coming-soon/img/bg_header_" + color + ".png)");
-			$("body").css("background-color", $(this).css("background-color"));
-			$("#progressbar").css("background-image", "url(themes/coming-soon/img/progressbar_fill_" + color + ".png)");
-			$("input[type=submit]").css("background-image", "url(themes/coming-soon/img/form_button_" + color + ".png)");
-		});
-		$(".bg_image").click(function () {
-			$("#background_image").removeClass("hidden");
-			$("body").addClass("active_bg_image");
-			$("header").css("background-image", "url(themes/coming-soon/img/bg_header_white.png)");
-			$("body").css("background-color", "#FFFFFF");
-			$("#progressbar").css("background-image", "url({themes/coming-soon/img/progressbar_fill_white.png)");
-			$("input[type=submit]").css("background-image", "url(themes/coming-soon/img/form_button_white.png)");
-		});
-		
-		/* Put your JavaScript here */
-			
-	});
-  </script>
-</head>
-
-<body>
-  <div id="pattern"><div id="gradient">
-
-	@yield('customization')
-
-
-    <!-- Image background -->
-    <img src="{{asset('themes/coming-soon/img/background.jpg')}}" id="background_image" alt="" class="hidden" />
-	<!-- Image background end -->
-	
-  
-	<!-- Main page -->
-    <div id="main_page">
-  
-	  <!-- Header -->
-	  <header>
-	    <h1>Muy pronto Presentate en la Web</h1> <!-- Set the page title -->	    
-	  </header>
-	  
-	  <!-- Additional content -->
-	  <div id="content"> &nbsp;	  
-		  
-		  
-		  <!-- Custom text -->		  
-		  @yield('text')
-		  <!-- Custom text end -->
-	  
-	  
-		  <!-- Countdown -->
-		  {{-- @yield('countdown') --}}
-		  <!-- Countdown end -->
-		  
-		  
-		  <!-- Progress -->
-		  @yield('progress')
-		  <!-- Progress end -->		  
-		  
-		  
-		  <!-- Short description -->
-		  @yield('shortDescription')		  
-		  <!-- Short description end -->
-		  
-		  
-		  <!-- Twitter widget -->
-		  {{-- @yield('twitterWidget') --}}
-		  <!-- Twitter widget end -->
-		  
-		  
-		  <!-- Subscription form -->
-		  @yield('suscriptionForm')
-		  <!-- Subscription form end -->
-		  
-		  
-		  <!-- More info, part 1 -->
-		  {{-- @yield('moreInfo') --}}
-		  <!-- More info, part 1 end -->
-		  
-		  
-	  </div>
-	  
-	</div>
-	<!-- Main page end -->	
-
-	<!-- More info, part 2 -->
-	{{-- @yield('moreInfoContent') --}}
-	<!-- More info, part 2 end -->
-
-  </div>
-  </div>
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-53298821-1', 'auto');
-  ga('send', 'pageview');
-</script>
-</body>
+	<body>
+	</body>
 </html>
