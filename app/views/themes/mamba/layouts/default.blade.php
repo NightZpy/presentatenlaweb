2a7b49da9b4d9f75d0393460bffe27d9b1b63b59
ajpadilla <ajpadilla88@gmail.com>
2014-08-04 23:06:05 -0430

    Agregando vista tipo mamba al proyecto

---
+++ app/views/themes/mamba/layouts/default.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html class="no-js">
+	<head>
+  		<meta charset="utf-8">
+		<!-- Page title -->
+  		<title>Presentate en la web</title> 
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    	<!-- css -->
+   	 	<link href="{{asset('themes/mamba/css/bootstrap.min.css')}}" rel="stylesheet" media="screen">
+    	<link href="{{asset('themes/mamba/css/style.css')}}" rel="stylesheet" media="screen">
+		<link href="{{asset('themes/mamba/color/default.css')}}" rel="stylesheet" media="screen">
+	 	
+		<!-- js -->
+		<script src="{{asset('themes/mamba/js/modernizr.custom.js')}}"></script>
+    	<script src="{{asset('themes/mamba/js/jquery.js')}}"></script>
+    	<script src="{{asset('themes/mamba/js/bootstrap.min.js')}}"></script>
+		<script src="{{asset('themes/mamba/js/jquery.smooth-scroll.min.js')}}"></script>
+		<script src="{{asset('themes/mamba/js/jquery.dlmenu.js')}}"></script>
+		<script src="{{asset('themes/mamba/js/wow.min.js')}}"></script>
+		<script src="{{asset('themes/mamba/js/custom.js')}}"></script>
+  	
+ 	</head>
+
+	<body>
+	</body>
+</html>
