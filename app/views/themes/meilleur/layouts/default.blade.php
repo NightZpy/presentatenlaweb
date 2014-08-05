c4fcf768474326e21c62c79a6fb11b92dc34492b
ajpadilla <ajpadilla88@gmail.com>
2014-08-05 19:18:02 -0430

    Agregando enlaces a hojas de estili css y archivos .js de la plantilla

---
+++ app/views/themes/meilleur/layouts/default.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>Meilleur - Free Responsive Single Page BS3 Template</title>
+    	
+		<!-- Stylesheets -->
+    	<link type="text/css" href="css/bootstrap.css" rel="stylesheet">
+    	<link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic,500italic,500,300italic,300'
+    	rel='stylesheet' type='text/css'>
+		<link type="text/css" href="icons/font-awesome/css/font-awesome.css" rel="stylesheet">
+    	<link type="text/css" href="icons/rondo/style.css" rel="stylesheet">
+    	<link href="css/jquery.fancybox.css" rel="stylesheet" type="text/css" />
+    	<link type="text/css" href="css/style.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<!--Scripts-->
+    	<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+   	 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    	<script type="text/javascript" src="js/jquery.smooth-scroll.min.js"></script>
+    	<script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
+    	<script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
+    	<script src="js/modernizr.js" type="text/javascript"></script>
+    	<script src="js/jquery.fancybox.pack.js" type="text/javascript"></script>
+    	<script type="text/javascript" src="js/custom.js"></script>
+	</body>
+</html>
