cf0333d6e5f2aa76a9e119441bd21b972944bf30
ajpadilla <ajpadilla88@gmail.com>
2014-08-05 21:37:32 -0430

    Plantill principal

---
+++ app/views/themes/meilleur/layouts/default.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<title>Meilleur - Free Responsive Single Page BS3 Template</title>
+    	
+		<!-- Stylesheets -->
+    	<link type="text/css" href="{{asset('themes/meilleur/css/bootstrap.css')}}" rel="stylesheet">
+    	<link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic,500italic,500,300italic,300'
+    	rel='stylesheet' type='text/css'>
+		<link type="text/css" href="{{asset('themes/meilleur/icons/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+    	<link type="text/css" href="{{asset('themes/meilleur/icons/rondo/style.css')}}" rel="stylesheet">
+    	<link href="{{asset('themes/meilleur/css/jquery.fancybox.css')}}" rel="stylesheet" type="text/css" />
+    	<link type="text/css" href="{{asset('themes/meilleur/css/style.css')}}" rel="stylesheet">
+	</head>
+
+	<body>
+		<header class="header" id="jump">
+       		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+           		<div class="container">
+               		<div class="navbar-header">
+                   		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                       		<span class="sr-only">Toggle navigation</span>
+                       		<span class="icon-bar"></span>
+                       		<span class="icon-bar"></span><span class="icon-bar"></span>
+                   		</button>
+                 		<a class="navbar-brand" href="#">
+                      		<img src="{{asset('themes/meilleur/img/logo.png')}}" alt="" />
+                  		</a>
+              		</div>
+  
+              		<div class="collapse navbar-collapse hidden-xs">
+                 		<ul class="nav navbar-nav navbar-right">
+                      		<li class="active"><a href="#jump0">Home</a></li>
+                     		<li><a href="#jump1">Services</a></li>
+                      		<li><a href="#jump2">Portfolio</a></li>
+                      		<li><a href="#jump3">Pricing</a></li>
+                      		<li><a href="#jump4">Our Team</a></li>
+                      		<li><a href="#jump5">Testimonials</a></li>
+                      		<li><a href="#jump6">Contact Us</a></li>
+                  		</ul>
+              		</div>
+              		<!-- /.navbar-collapse -->
+              		<div class="collapse navbar-responsive-collapse">
+                  		<ul class="nav navbar-nav">
+                      		<li class="active"><a href="#jump0" data-toggle="collapse" data-target=".navbar-responsive-collapse">Home</a></li>
+                      		<li><a href="#jump1" data-toggle="collapse" data-target=".navbar-responsive-collapse">Services</a></li>
+                      		<li><a href="#jump2" data-toggle="collapse" data-target=".navbar-responsive-collapse">Portfolio</a></li>
+                      		<li><a href="#jump3" data-toggle="collapse" data-target=".navbar-responsive-collapse">Pricing</a></li>
+                      		<li><a href="#jump4" data-toggle="collapse" data-target=".navbar-responsive-collapse">Team</a></li>
+                      		<li><a href="#jump5" data-toggle="collapse" data-target=".navbar-responsive-collapse">Clients</a></li>
+                      		<li><a href="#jump6" data-toggle="collapse" data-target=".navbar-responsive-collapse">Contact</a></li>
+                  		</ul>
+              		</div>
+              		<!-- /.navbar-responsive-collapse -->
+          		</div>
+      		</nav>
+  		</header>
+		<div class="jumper" id="jump0">
+    	</div>
+
+    	<div class="section type-1 big splash">
+        	<div class="splash-cover">
+        	</div>
+
+        	<div class="container">
+            	<div class="splash-block">
+                	<div class="centered">
+                    	<div class="container">
+                       		<div class="section-headlines">
+                            	<h1>Meilleur - A Bootstrap 3 based single page responsive free template</h1>
+                            	<p>Finally, a free website template - Easier, Prettier, Smarter and Fun!</p>
+                        	</div>
+                        	<a href="#jump2" class="btn btn-outline btn-lg">Our Portfolio </a>&nbsp; 
+							<a href="#jump6" class="btn btn-outline btn-lg">Hire Us </a>
+                    	</div>
+                	</div>
+            	</div>
+        	</div>
+    	</div>
+		
+		@yield('jump1')
+		
+		<!--Scripts-->
+    	<script type="text/javascript" src="{{asset('themes/meilleur/js/jquery-1.10.2.min.js')}}"></script>
+   	 	<script type="text/javascript" src="{{asset('themes/meilleur/js/bootstrap.min.js')}}"></script>
+    	<script type="text/javascript" src="{{asset('themes/meilleur/js/jquery.smooth-scroll.min.js')}}"></script>
+    	<script type="text/javascript" src="{{asset('themes/meilleur/js/jquery.mixitup.min.js')}}"></script>
+    	<script src="{{asset('themes/meilleur/js/jquery.easing.1.3.js')}}" type="text/javascript"></script>
+    	<script src="{{asset('themes/meilleur/js/modernizr.js')}}" type="text/javascript"></script>
+    	<script src="{{asset('themes/meilleur/js/jquery.fancybox.pack.js')}}" type="text/javascript"></script>
+    	<script type="text/javascript" src="{{asset('themes/meilleur/js/custom.js')}}"></script>
+	</body>
+</html>
