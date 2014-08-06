0a6f1a2b865935d055ccebf61ee5b4ac3ced57f9
ajpadilla <ajpadilla88@gmail.com>
2014-08-05 19:31:03 -0430

    Agregando ruta para las hojas de estilo css y archivos .js

--- app/views/themes/meilleur/layouts/default.blade.php
+++ app/views/themes/meilleur/layouts/default.blade.php
@@ -7,24 +7,24 @@
     	<title>Meilleur - Free Responsive Single Page BS3 Template</title>
     	
 		<!-- Stylesheets -->
-    	<link type="text/css" href="css/bootstrap.css" rel="stylesheet">
+    	<link type="text/css" href="{{asset('themes/meilleur/css/bootstrap.css')}}" rel="stylesheet">
     	<link href='http://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic,500italic,500,300italic,300'
     	rel='stylesheet' type='text/css'>
-		<link type="text/css" href="icons/font-awesome/css/font-awesome.css" rel="stylesheet">
-    	<link type="text/css" href="icons/rondo/style.css" rel="stylesheet">
-    	<link href="css/jquery.fancybox.css" rel="stylesheet" type="text/css" />
-    	<link type="text/css" href="css/style.css" rel="stylesheet">
+		<link type="text/css" href="{{asset('themes/meilleur/icons/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+    	<link type="text/css" href="{{asset('themes/meilleur/icons/rondo/style.css')}}" rel="stylesheet">
+    	<link href="{{asset('themes/meilleur/css/jquery.fancybox.css')}}" rel="stylesheet" type="text/css" />
+    	<link type="text/css" href="{{asset('themes/meilleur/css/style.css')}}" rel="stylesheet">
 	</head>
 
 	<body>
 		<!--Scripts-->
-    	<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
-   	 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-    	<script type="text/javascript" src="js/jquery.smooth-scroll.min.js"></script>
-    	<script type="text/javascript" src="js/jquery.mixitup.min.js"></script>
-    	<script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
-    	<script src="js/modernizr.js" type="text/javascript"></script>
-    	<script src="js/jquery.fancybox.pack.js" type="text/javascript"></script>
-    	<script type="text/javascript" src="js/custom.js"></script>
+    	<script type="text/javascript" src="{{asset('themes/meilleur/js/jquery-1.10.2.min.js')}}"></script>
+   	 	<script type="text/javascript" src="{{asset('themes/meilleur/js/bootstrap.min.js')}}"></script>
+    	<script type="text/javascript" src="{{asset('themes/meilleur/js/jquery.smooth-scroll.min.js')}}"></script>
+    	<script type="text/javascript" src="{{asset('themes/meilleur/js/jquery.mixitup.min.js')}}"></script>
+    	<script src="{{asset('themes/meilleur/js/jquery.easing.1.3.js')}}" type="text/javascript"></script>
+    	<script src="{{asset('themes/meilleur/js/modernizr.js')}}" type="text/javascript"></script>
+    	<script src="{{asset('themes/meilleur/js/jquery.fancybox.pack.js')}}" type="text/javascript"></script>
+    	<script type="text/javascript" src="{{asset('themes/meilleur/js/custom.js')}}"></script>
 	</body>
 </html>
