5d12939f9465330354bd1cafde5203c61f84b795
ajpadilla <ajpadilla88@gmail.com>
2014-08-05 19:46:20 -0430

    CAmbiando a la plantilla blade principal el menu

--- app/views/themes/meilleur/layouts/default.blade.php
+++ app/views/themes/meilleur/layouts/default.blade.php
@@ -17,6 +17,49 @@
 	</head>
 
 	<body>
+		<header class="header" id="jump">
+       		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+           		<div class="container">
+               		<div class="navbar-header">
+                   		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                       		<span class="sr-only">Toggle navigation</span>
+                       		<span class="icon-bar"></span>
+                       		<span class="icon-bar"></span><span class="icon-bar"></span>
+                   		</button>
+                 		<a class="navbar-brand" href="#">
+                      		<img src="img/logo.png" alt="" />
+                  		</a>
+              		</div>
+  
+              		<div class="collapse navbar-collapse hidden-xs">
+                 		<ul class="nav navbar-nav navbar-right">
+                      		<li class="active"><a href="#jump0">Home</a></li>
+                     		<li><a href="#jump1">Services</a></li>
+                      		<li><a href="#jump2">Portfolio</a></li>
+                      		<li><a href="#jump3">Pricing</a></li>
+                      		<li><a href="#jump4">Our Team</a></li>
+                      		<li><a href="#jump5">Testimonials</a></li>
+                      		<li><a href="#jump6">Contact Us</a></li>
+                  		</ul>
+              		</div>
+              		<!-- /.navbar-collapse -->
+              		<div class="collapse navbar-responsive-collapse">
+                  		<ul class="nav navbar-nav">
+                      		<li class="active"><a href="#jump0" data-toggle="collapse" data-target=".navbar-responsive-collapse">Home</a></li>
+                      		<li><a href="#jump1" data-toggle="collapse" data-target=".navbar-responsive-collapse">Services</a></li>
+                      		<li><a href="#jump2" data-toggle="collapse" data-target=".navbar-responsive-collapse">Portfolio</a></li>
+                      		<li><a href="#jump3" data-toggle="collapse" data-target=".navbar-responsive-collapse">Pricing</a></li>
+                      		<li><a href="#jump4" data-toggle="collapse" data-target=".navbar-responsive-collapse">Team</a></li>
+                      		<li><a href="#jump5" data-toggle="collapse" data-target=".navbar-responsive-collapse">Clients</a></li>
+                      		<li><a href="#jump6" data-toggle="collapse" data-target=".navbar-responsive-collapse">Contact</a></li>
+                  		</ul>
+              		</div>
+              		<!-- /.navbar-responsive-collapse -->
+          		</div>
+      		</nav>
+  		</header>
+		
+
 		<!--Scripts-->
     	<script type="text/javascript" src="{{asset('themes/meilleur/js/jquery-1.10.2.min.js')}}"></script>
    	 	<script type="text/javascript" src="{{asset('themes/meilleur/js/bootstrap.min.js')}}"></script>
