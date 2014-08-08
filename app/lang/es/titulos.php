8b28fb7a8694843532da0d762c45a434707efc42
ajpadilla <ajpadilla88@gmail.com>
2014-08-07 23:46:22 -0430

    Agregando titulos del contenido de la pagina

---
+++ app/lang/es/titulos.php
@@ -0,0 +1,12 @@
+<?php	
+	//app/lang/es/titulos.php
+   
+return array(
+	"our_services" => "Nuestros Sevicios",
+	"websites"=>"Sitios Webs",
+ 	"web_design"=>"Diseño de Páginas Web",
+	"mobile_applications"=>"Aplicaciones Móviles",
+ 	"hosting _and_Domains"=>"Hosting y Dominios"
+  );
+~                                                                                                                                                      
+~           
