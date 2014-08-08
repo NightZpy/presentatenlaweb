b1e54544909441bec8cfb35875753894cec59e0f
ajpadilla <ajpadilla88@gmail.com>
2014-08-07 23:47:07 -0430

    Agregando el texto para para cada titulo del contenido de la pagina

---
+++ app/lang/es/textos.php
@@ -0,0 +1,11 @@
+<?php
+    //app/lang/es/textos.php
+ 
+return array(
+    "our_services" => "Nos encanta la creación de estrategias innovadoras para la web. Todo tipo de empresas nos contratan: hacemos las cosas difíciles fácil, lo imposible lo hacemos realidad; pero nunca tomamos nada a la ligera.",
+
+	"websites"=>"Aplicaciones Web a la medida para optimizar su empresa. Convertimos sus necesidades e ideas en una solución Web que dara verdaderos resultados.",
+	"web_design"=>"Sitios Web especializados en cumplir sus objetivos. Basado en un proceso artístico y de ingeniería con las últimas tendencias de Internet",
+	"mobile_applications"=>"El mundo ahora es móvil, su empresa en las manos de todos. Convertimos su necesidad e idea en una app para iPhone, iPad y Android.",
+	"hosting _and_Domains"=>"Servidores propios con soporte especializado y en español. La mejor velocidad y tiempo en línea de 99.9%"
+);
