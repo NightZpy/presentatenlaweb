fbf71fc3f02ff6d7f244394b16a66d717e738110
ajpadilla <ajpadilla88@gmail.com>
2014-08-11 23:52:24 -0430

    Agregando traducciones usando la funcion trans al blade

--- app/views/themes/meilleur/pages/home.blade.php
+++ app/views/themes/meilleur/pages/home.blade.php
@@ -3,289 +3,144 @@
 @section('our_services')
 	<div id="jump1" class="jumper">
 	</div>
-    <div class="section type-2">
-  		<div class="container">
-       		<div class="section-headlines">
-           		<h4>Our Services</h4>
-               	<div>
-                    We love creating innovative strategies for the web. All kind of brands hire us:
-                    we make difficult things easy, but never take anything lightly.
+	<div class="section type-2">
+        <div class="container">
+            <div class="section-headlines">
+                <h4>{{ trans('titulos.our_services') }}</h4>
+                <div>
+					{{ trans('textos.our_services') }}
                 </div>
             </div>
             <div class="row">
-           		<div class="col-lg-6 features">
-                	<div class="media">
-                   		<i class=" icon-wrench pull-left animated bounce"></i>
+                <div class="col-lg-6 features">
+                    <div class="media">
+                        <i class=" icon-wrench pull-left animated bounce"></i>
                         <div class="media-body">
-                       	<h3>Websites</h3>
-                        <div>Contrary to popular belief, Lorem Ipsum is not simply random text.</div>
-               		</div>
-        		</div>
-
-           	 	<br class="gap-30" />
-              	<div class="media">
-               		<i class=" icon-bar-chart pull-left animated bounce"></i>
-                   	<div class="media-body">
-                    <h3>Social Media Campaigns</h3>
-                  	<div>The point of using Lorem Ipsum is that it has a more-or-less normal dist.</div>
-               	</div>
-     		</div>
-			<br class="gap-30" />
-        	<div class="media">
-            	<i class=" icon-mobile-phone pull-left animated bounce"></i>
-          		<div class="media-body">
-					<h3>iOS and Android Apps</h3>
-					<div>
-                                It is a long established fact that a reader will be distracted by the readable content
-                                of a page when looking at its layout.
-					</div>
-       			</div>
-     		</div>
-        	<div class="visible-xs visible-sm">
-				<br class="gap-30" />
-				<hr class="gap-divider" />
-				<br class="gap-30" />	
-			</div>
-		</div>
-		<div class="col-lg-6 features">
-			<div class="panel-group" id="accordion">
-         		<div class="panel">
-             		<div class="panel-heading">
-                   		 <h3 class="panel-title">
-                       		<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-                            	Responsive Layout <i class="icon-plus"></i><i class="icon-minus"></i>
-							</a>
-
-                        </h3>
-             		</div>
-					<div id="collapseOne" class="panel-collapse collapse in">
-						<div class="panel-body">
-							Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
-							in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-							<br/>
-							<br/>
-							Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked
-							up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
-                            and going through the cites of the word in classical literature, discovered the
-							undoubtable source.
-						</div>
-					</div>
-				</div>
-				<div class="panel">
-					<div class="panel-heading">
-						<h3 class="panel-title">
-							<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
-                             	href="#collapseTwo">Retina Ready <i class="icon-plus"></i>
-								<i class="icon-minus"></i>
-							</a>
-                    	</h3>
-
-                	</div>
-             		<div id="collapseTwo" class="panel-collapse collapse">
-             			<div class="panel-body">
-                   			Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
-                         	in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-                                    <br />
-                                    <br />
-                                    Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked
-                                    up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
-
-                                    and going through the cites of the word in classical literature, discovered the
-
-                                    undoubtable source.
-
-                                </div>
-
-                            </div>
-
+                            <h3>{{ trans('titulos.websites') }}</h3>
+                            <div>{{ trans('textos.websites') }}</div>
                         </div>
-
-                        <div class="panel">
-
-                            <div class="panel-heading">
-
-                                <h3 class="panel-title">
-
-                                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion"
-
-                                        href="#collapseThree">Built Using Bootstrap 3 <i class="icon-plus"></i><i class="icon-minus">
-
-                                        </i></a>
-
-                                </h3>
-
-                            </div>
-
-                            <div id="collapseThree" class="panel-collapse collapse">
-
-                                <div class="panel-body">
-
-                                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
-
-                                    in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-
-                                    Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked
-
-                                    up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
-
-                                    and going through the cites of the word in classical literature, discovered the
-
-                                    undoubtable source.
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
                     </div>
+                    <br class="gap-30">
+                    <div class="media">
+                        <i class=" icon-bar-chart pull-left animated bounce"></i>
+                        <div class="media-body">
+                            <h3>{{ trans('titulos.web_design') }}</h3>
+                            <div>{{ trans('textos.web_design') }}</div>
+                        </div>
+                    </div>
+                    <br class="gap-30">
+                    <div class="media">
+                        <i class=" icon-mobile-phone pull-left animated bounce"></i>
+                        <div class="media-body">
+                            <h3>{{ trans('titulos.mobile_applications') }}</h3>
+                            <div>{{ trans('textos.mobile_applications') }}</div>
+                        </div>
+                    </div>
+					<br class="gap-30">
+					<div class="media">
+                  		<i class=" icon-bar-chart pull-left animated bounce"></i>
+                   		<div class="media-body">
+                      		<h3>{{ trans('titulos.hosting _and_Domains') }}</h3>
+                        	<div>{{ trans('textos.hosting _and_Domains') }}</div>
+                       		</div>
+                       </div>
 
                     <div class="visible-xs visible-sm">
-
-                        <br class="gap-30" />
-
-                        <hr class="gap-divider" />
-
-                        <br class="gap-30" />
-
+                        <br class="gap-30">
+                        <hr class="gap-divider">
+                        <br class="gap-30">
                     </div>
-
                 </div>
-
+                <div class="col-lg-6 features">
+                    <div class="panel-group" id="accordion">
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">
+                                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                        				{{ trans('titulos.Responsive_Layout') }}<i class="icon-plus"></i><i class="icon-minus"></i></a>
+                                </h3>
+                            </div>
+                            <div id="collapseOne" class="panel-collapse collapse" style="height: 0px;">
+                                <div class="panel-body">
+									{{ trans('textos.Responsive_Layout') }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">
+                                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">{{ trans('titulos.Built_Using_Bootstrap_3') }}<i class="icon-plus"></i><i class="icon-minus">
+                                        </i></a>
+                                </h3>
+                            </div>
+                            <div id="collapseTwo" class="panel-collapse collapse" style="height: 0px;">
+                                <div class="panel-body">
+									{{ trans('textos.Built_Using_Bootstrap_3') }}
+                             	</div>
+                            </div>
+                        </div>
+                        <!--<div class="panel">
+                            <div class="panel-heading">
+                                <h3 class="panel-title">
+                                    <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">{{ trans('') }} <i class="icon-plus"></i><i class="icon-minus">
+                                        </i></a>
+                                </h3>
+                            </div>
+                            <div id="collapseThree" class="panel-collapse collapse" style="height: 0px;">
+                                <div class="panel-body">
+									{{ trans('') }}
+                                </div>
+                            </div>
+                        </div>-->
+                    </div>
+                    <div class="visible-xs visible-sm">
+                        <br class="gap-30">
+                        <hr class="gap-divider">
+                        <br class="gap-30">
+                    </div>
+                </div>
             </div>
-
             <br>
-
             <br>
-
             <br>
-
             <div class="row">
-
                 <div class="col-lg-6 features">
-
-                    <h3>
-
-                        Our Skills</h3>
-
+                    <h3>{{ trans('titulos.Our_Skills') }}</h3>
                     <div class="skills">
-
-                        <p>
-
-                            HTML5 and CSS3</p>
-
+                        <p>{{ trans('titulos.HTML5_and_CSS3') }}</p>
                         <div class="progress">
-
-                            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0"
-
-                                aria-valuemax="100" style="width: 70%;">
-
+                            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 70%;">
                             </div>
-
                         </div>
-
                         <hr>
-
-                        <p>
-
-                            Mobile Apps Development</p>
-
+                        <p>{{ trans('titulos.Mobile_Apps_Development') }}</p>
                         <div class="progress">
-
-                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60"
-
-                                aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-
+                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
                             </div>
-
                         </div>
-
                         <hr>
-
-                        <p>
-
-                            Software Development</p>
-
+                        <p>{{ trans('titulos.Software_Development') }}</p>
                         <div class="progress">
-
-                            <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60"
-
-                                aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
-
+                            <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">
                             </div>
-
                         </div>
-
                         <hr>
-
-                        <p>
-
-                            Search Engine Optimization</p>
-
+                        <p>{{ trans('titulos.Javascript') }}</p>
                         <div class="progress">
-
-                            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60"
-
-                                aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-
+                            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
                             </div>
-
                         </div>
-
                     </div>
-
                 </div>
-
                 <div class="col-lg-6 features">
-
-                    <h3>
-
-                        Get To Now Us</h3>
-
+                    <h3>{{ trans('titulos.Get_To_Know_Us') }}</h3>
                     <p>
-
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots
-
-                        in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
-
-                        Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked
-
-                        up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
-
-                        and going through the cites of the word in classical literature, discovered the
-
-                        undoubtable source.
-
-                    </p>
-
-                    <p>
-
-                        Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
-
-                        (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
-
-                        on the theory of ethics, very popular during the Renaissance. The first line of
-
-                        Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-                    </p>
-
-                    <p>
-
-                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those
-
-                        interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by
-
-                        Cicero are also reproduced in their exact original form, accompanied by English
-
-                        versions from the 1914 translation by H. Rackham.</p>
-
-                </div>
-
+						{{ trans('textos.Get_To_Know_Us') }}
+                   	</p>
+           		</div>
             </div>
-
         </div>
-	</div>
+    </div>
 @stop
 
 @section('featured_works')
