f2f4df68ca5b076c0121057d6a1508dae809d473
ajpadilla <ajpadilla88@gmail.com>
2014-08-11 23:47:08 -0430

    Agregando nuevos titulos al array

--- app/lang/es/titulos.php
+++ app/lang/es/titulos.php
@@ -1,12 +1,19 @@
 <?php
  
 return array(
-    "our_services" => "Titulo 1",
-
-	"websites" => "Titulo 2",
-	"web_design" => "Titulo 3",
-	"mobile_applications" => "Titulo 4",
-	"hosting _and_Domains" => "Titulo 5",
+    "our_services" => "NUESTROS SERVICIOS",
+	"websites" => "Aplicaciones Web",
+	"web_design" => "Diseño de Páginas Web",
+	"mobile_applications" => "Aplicaciones Móviles",
+	"hosting _and_Domains" => "Hosting y Dominios",
+    "Our_Skills" => "Nuestras Habilidades",
+	"HTML5_and_CSS3" => "HTML5 y CSS3",	
+	"Javascript" => "Javascript",
+	"Mobile_Apps_Development" => "Desarrollo de Aplicaciones Móviles",
+	"Software_Development" => "Desarrollo de software",
+	"Responsive_Layout" => "Diseño sensible", 
+	"Built_Using_Bootstrap_3"=>"Construimos usando Bootstrap 3",
+	"Get_To_Know_Us"=>"Conózcanos"
 );
 
 ?>
