d740bfe8ed384ab1e8d6ef459fa20e17a445dc3e
ajpadilla <ajpadilla88@gmail.com>
2014-08-11 23:46:35 -0430

    Agregando nuevas traducciones para los textos ala array

--- app/lang/es/textos.php
+++ app/lang/es/textos.php
@@ -8,6 +8,9 @@ return array(
 	"web_design" => "Sitios Web especializados en cumplir sus objetivos. Basado en un proceso artístico y de ingeniería con las últimas tendencias de Internet",
 	"mobile_applications" => "El mundo ahora es móvil, su empresa en las manos de todos. Convertimos su necesidad e idea en una app para iPhone, iPad y Android.",
 	"hosting _and_Domains" => "Servidores propios con soporte especializado y en español. La mejor velocidad y tiempo en línea de 99.9%",
+	"Responsive_Layout" => "Nuestros diseños de sitios web se adapta a los diferentes dispositivos con el fin de lograr la mejor experiencia en cada uno de ellos.",
+	"Built_Using_Bootstrap_3"=>"El framework de Twitter que permite crear interfaces web con CSS y Javascript que se adaptan al tamaño de los diferentes dispositivos",
+	"Get_To_Know_Us"=>"Hacemos crecer su empresa en internert de forma rápida y efectiva con tecnología Web y Móvil de primera. Contamos con gran cantidad de proyectos exitosos realizados, caracterizándonos por ser una compañía responsable con un alto nivel de innovación y altos estándares de calidad"
 );
 
 ?>
