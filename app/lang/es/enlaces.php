f615cb289276fc3a44e021ebe03876603b153ca2
ajpadilla <ajpadilla88@gmail.com>
2014-08-12 01:30:38 -0430

    Agregando traduccion de enlaces para la barra de navegacion

--- app/lang/es/enlaces.php
+++ app/lang/es/enlaces.php
@@ -3,7 +3,15 @@
  
 return array(
 	"Our_Portfolio" => "Nuestro Portafolio",
-	"Contact_Us"=>"Contactenos"
+	"Contact_Us"=>"Contactenos",
+	"Home"=>"Inicio",
+	"Services"=>"Servicios",
+	"Portfolio"=>"Portfafolio",
+    "Pricing"=>"Precios",
+	"Contact_Us"=>"Contáctenos",
+	"languages"=>"Idiomas",
+	"Spanish"=>"Español",
+	"English"=>"Ingles",
 );
 
 ?>
