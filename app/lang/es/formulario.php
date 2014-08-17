3632e4d661d74683d57ffd6c12948acfb5607184
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-08-17 14:11:40 -0430

    Traducciendo mensajes de error y enviado para los correos

--- app/lang/es/formulario.php
+++ app/lang/es/formulario.php
@@ -4,6 +4,8 @@ return array(
 	"Your_Email" => "Tu Email",
 	"Subject"=> "Asunto",
 	"Send_Message_Now" => "Enviar Mensaje",
+	"Success" => "Tu mensaje ha sido enviado correctamente!.",
+	"Error"=>"No se puede enviar el mensaje, por favor intente más tarde.",
 	"Sending_E_mail" => "Enviando Mensaje...",
 	"Send_E_mail"=>"Enviar Mensaje"
 );
