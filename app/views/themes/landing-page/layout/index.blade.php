b6112925288d86329605d365b23f77636b088d08
night zpy <nightzpy@nightzpy.nightzpy.com>
2014-08-18 00:25:51 -0430

    Agregando nueva seccion al index

--- app/views/themes/landing-page/layout/index.blade.php
+++ app/views/themes/landing-page/layout/index.blade.php
@@ -74,13 +74,16 @@
                     <div class="intro-message">
                         <h1>Eduktivo</h1>
                         <h3>Sistema De Control De Procesos Escolares</h3>
+                        <p>Intiza te ayudará a gestionar eficientemente
+las cobranzas de tu empresa. Así podrás
+cobrar más rápido por tus ventas.</p>
                         <hr class="intro-divider">
                         <ul class="list-inline intro-social-buttons">
                             <li>
                                 <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
                             </li>
                             <li>
-                                <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                                <!--<a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>-->
                             </li>
                             <li>
                                 <a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
@@ -163,6 +166,28 @@
 
     </div>
     <!-- /.content-section-a -->
+
+
+    <div class="content-section-b">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                    <hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">3D Device Mockups<br>by PSDCovers</h2>
+                    <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
+                </div>
+                <div class="col-lg-5 col-sm-pull-6  col-sm-6">
+                    <img class="img-responsive" src="{{asset('themes/landing-page/img/dog.png')}}" alt="">
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+
 
     <div class="banner">
 
