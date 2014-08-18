6d4e917a5e8d58c9dbbc1fae753ad182220afc06
Lenyn Alcantara (NightZpy) <nightzpy@gmail.com>
2014-08-17 22:06:29 -0430

    media queries para las imágenes del logo

--- app/views/themes/meilleur/layouts/default.blade.php
+++ app/views/themes/meilleur/layouts/default.blade.php
@@ -27,7 +27,7 @@
                        		<span class="icon-bar"></span><span class="icon-bar"></span>
                    		</button>
                  		<a class="navbar-brand" href="#">
-                      		<img src="{{asset('themes/meilleur/img/logo.png')}}" alt="" />
+                      		<div id="logo"></div>
                   		</a>
               		</div>
   
