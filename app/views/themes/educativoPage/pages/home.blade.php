d8ce247f4bcdfc98cd66f44443a949aa4124f3df
ajpadilla <ajpadilla88@gmail.com>
2014-08-21 19:12:03 -0430

    Agregando seccion cuenta_administrador

--- app/views/themes/educativoPage/pages/home.blade.php
+++ app/views/themes/educativoPage/pages/home.blade.php
@@ -7,13 +7,13 @@
             	<div class="col-lg-5 col-sm-6">
                 	<hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">{{ trans('textos.features_educativo') }}</h2>
+                    <h2 class="section-heading">{{ trans('titulos.features_educativo') }}</h2>
                     <p class="lead">{{trans('textos.educativo')}}</p>
 					<ul>
-						<li>1. {{trans('textos.manager')}}</li>
-						<li>2. {{trans('textos.teacher')}}</li>
-						<li>3. {{trans('textos.student')}}</li>
-						<li>4. {{trans('textos.parent')}}</li>
+						<li>1. {{trans('titulos.manager')}}</li>
+						<li>2. {{trans('titulos.teacher')}}</li>
+						<li>3. {{trans('titulos.student')}}</li>
+						<li>4. {{trans('titulos.parent')}}</li>
 					</ul>
                 </div>
                 <div class="col-lg-5 col-lg-offset-2 col-sm-6">
@@ -22,5 +22,24 @@
             </div>
         </div>
        <!-- /.container -->
+    </div>
+@stop()
+
+@section('cuenta_administrador')
+	<div class="content-section-b">
+    	<div class="container">
+        	<div class="row">
+            	<div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                	<hr class="section-heading-spacer">
+                    <div class="clearfix"></div>
+                    <h2 class="section-heading">3D Device Mockups<br>by PSDCovers</h2>
+                    <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
+                </div>
+                <div class="col-lg-5 col-sm-pull-6  col-sm-6">
+                    <img class="img-responsive" src="img/dog.png" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
     </div>
 @stop()
