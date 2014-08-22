446c3200ff4ef58171685ecb03c89e4602cd7e8c
ajpadilla <ajpadilla88@gmail.com>
2014-08-21 22:14:58 -0430

    Agregando traduccuion de las caracteristicas del sistema educativo

--- app/views/themes/educativoPage/pages/home.blade.php
+++ app/views/themes/educativoPage/pages/home.blade.php
@@ -32,11 +32,47 @@
             	<div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
                 	<hr class="section-heading-spacer">
                     <div class="clearfix"></div>
-                    <h2 class="section-heading">3D Device Mockups<br>by PSDCovers</h2>
-                    <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by <a target="_blank" href="http://www.psdcovers.com/">PSDCovers</a>! Visit their website to download some of their awesome, free photoshop actions!</p>
+                    <h2 class="section-heading">{{trans('titulos.account_administrator')}}</h2>
+					<ul>
+						<li>{{trans('caracteristicas.manage_students_class_group_wise')}}</li>
+						<li>{{trans('caracteristicas.add_edit_delete_student')}}</li>
+						<li>{{trans('caracteristicas.view_profile_of_students')}}</li>
+						<li>{{trans('caracteristicas.view_marksheet_of_sudent')}}</li>
+						<li>{{trans('caracteristicas.view_ID_card_of_student')}}</li>
+						<li>{{trans('caracteristicas.Manage_teacher_profile')}}</li>
+						<li>{{trans('caracteristicas.Add_edit_delete_teacher_information')}}</li>
+                        <li>{{trans('caracteristicas.Manage_parent_according_to_student_classwise')}}</li>
+						<li>{{trans('caracteristicas.Create_edit_delete_parent_information')}}</li>
+                        <li>{{trans('caracteristicas.Create_edit_delete_class_group_for_students')}}</li>
+						<li>{{trans('caracteristicas.Create_edit_delete_subjects_here_from_subject_menu')}}</li>
+                        <li>{{trans('caracteristicas.Subjects_can_be_defined_seperately_according_to_each_classes')}}</li>
+						<li>{{trans('caracteristicas.Manage_exam_semester_listing')}}</li>
+                        <li>{{trans('caracteristicas.Manage_marks_edit_update_and_attendance_exam_class_student_wise')}}</li>
+						<li>{{trans('caracteristicas.Manage_exam_grades')}}</li>
+                       	<li>{{trans('caracteristicas.Manage_class_routine')}}</li>
+						<li>{{trans('caracteristicas.Create_edit_delete_class_routine_schedule_on_7days_a_week')}}</li>
+                        <li>{{trans('caracteristicas.Manage_payment_for_student')}}</li>
+						<li>{{trans('caracteristicas.Create_edit_delete_invoice_listing')}}</li>
+                        <li>{{trans('caracteristicas.View_invoice_and_print_them')}}</li>
+						<li>{{trans('caracteristicas.Manage_library')}}</li>
+                        <li>{{trans('caracteristicas.Create_edit_delete_booklist')}}</li>
+						<li>{{trans('caracteristicas.Manage_transportation_routes_for_school')}}</li>
+                        <li>{{trans('caracteristicas.Manage_dormitory_listing_for_school')}}</li>
+						<li>{{trans('caracteristicas.Manage_noticeboard_of_school')}}</li>
+                        <li>{{trans('caracteristicas.Create_edit_delete_notices_according_to_date')}}</li>
+						
+						<li>{{trans('caracteristicas.Notices_are_visible_in_calendar_in_dashboard')}}</li>
+                        <li>{{trans('caracteristicas.Edit_system_settings')}}</li>
+                        <li>{{trans('caracteristicas.Paypal_email_in_system_settings_is_paypal_payment_gateway_for_student_invoice_online_payments')}}</li>
+                        <li>{{trans('caracteristicas.Manage_language_of_the_software')}}</li>
+                        <li>{{trans('caracteristicas.Create_your_own_language_and_convert_the_language_phrases_easily')}}</li>
+                        <li>{{trans('caracteristicas.Create_backup_and_restore_anytime')}}</li>
+                        <li>{{trans('caracteristicas.Manage_own_profile')}}</li>
+                        <li>{{trans('caracteristicas.Access_account_from_anywhere_by_any_device_like_desktop_laptop_smartphone_and_tablet')}}</li>		
+					</ul>
                 </div>
                 <div class="col-lg-5 col-sm-pull-6  col-sm-6">
-                    <img class="img-responsive" src="img/dog.png" alt="">
+                    <img class="img-responsive" src="{{asset('themes/educativo-page/img/dog.png')}}" alt="">
                 </div>
             </div>
         </div>
